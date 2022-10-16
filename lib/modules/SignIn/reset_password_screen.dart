22f331846069c5587e5108a2400b2270efe6496f
alghoraqy <mahmoudalghoraqy@gmail.com>
2022-10-16 05:08:15 +0200

    last

--- lib/modules/SignIn/reset_password_screen.dart
+++ lib/modules/SignIn/reset_password_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mega_store/bloc/Login/login_cubit.dart';
@@ -27,91 +28,94 @@ class ResetPasswordScreen extends StatelessWidget {
               padding: EdgeInsets.only(top: rheight(context) / 30),
               child: SingleChildScrollView(
                 physics: const BouncingScrollPhysics(),
-                child: Column(
-                  children: [
-                    const LogoWithText(
-                        firstText: AppString.resetPassword, secondText: ''),
-                    SizedBox(
-                      height: rheight(context) / 30,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: rwidth(context) / 25,
-                          vertical: rheight(context) / 30),
-                      child: Form(
-                        key: cubit.resetPasswordformKey,
-                        child: Column(
-                          children: [
-                            MyFormField(
-                                hint: 'New Password',
-                                prefix: Icons.lock_outline_rounded,
-                                controller: cubit.resetpasswordController,
-                                isPassword: cubit.isPassword1,
-                                suffix: cubit.isPassword1
-                                    ? Icons.visibility_off_outlined
-                                    : Icons.visibility_outlined,
-                                suffixPress: () {
-                                  cubit.changeVisabilty(1);
-                                },
-                                validate: (value) {
-                                  if (value!.isEmpty) {
-                                    return AppString.emptyPassword;
-                                  } else {
-                                    if (value.length < 6) {
-                                      return AppString.notValidPassword;
-                                    }
-                                  }
-                                  return null;
-                                },
-                                inputType: TextInputType.visiblePassword),
-                            SizedBox(
-                              height: rheight(context) / 50,
-                            ),
-                            MyFormField(
-                                hint: 'Confirm Password',
-                                prefix: Icons.lock_outline_rounded,
-                                controller:
-                                    cubit.confirmResetpasswordController,
-                                isPassword: cubit.isPassword2,
-                                suffix: cubit.isPassword2
-                                    ? Icons.visibility_off_outlined
-                                    : Icons.visibility_outlined,
-                                suffixPress: () {
-                                  cubit.changeVisabilty(2);
-                                },
-                                validate: (value) {
-                                  if (value!.isEmpty) {
-                                    return AppString.emptyPassword;
-                                  } else {
-                                    if (cubit.confirmResetpasswordController
-                                            .text !=
-                                        cubit.resetpasswordController.text) {
-                                      return AppString.notValidConfirmPassword;
-                                    }
-                                  }
-                                  return null;
-                                },
-                                inputType: TextInputType.visiblePassword),
-                            SizedBox(
-                              height: rheight(context) / 10,
-                            ),
-                            MyButton(
-                                child: Text(
-                                  'Conirm',
-                                  style: AppTextStyle.buttonText,
-                                ),
-                                onPressed: () {
-                                  if (cubit.resetPasswordformKey.currentState!
-                                      .validate()) {
-                                    navigatAndReplacement(context,
-                                        screen: const SignInScreen());
-                                  }
-                                })
-                          ],
-                        ),
+                child: FadeInRight(
+                  child: Column(
+                    children: [
+                      const LogoWithText(
+                          firstText: AppString.resetPassword, secondText: ''),
+                      SizedBox(
+                        height: rheight(context) / 30,
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: rwidth(context) / 25,
+                            vertical: rheight(context) / 30),
+                        child: Form(
+                          key: cubit.resetPasswordformKey,
+                          child: Column(
+                            children: [
+                              MyFormField(
+                                  hint: 'New Password',
+                                  prefix: Icons.lock_outline_rounded,
+                                  controller: cubit.resetpasswordController,
+                                  isPassword: cubit.isPassword1,
+                                  suffix: cubit.isPassword1
+                                      ? Icons.visibility_off_outlined
+                                      : Icons.visibility_outlined,
+                                  suffixPress: () {
+                                    cubit.changeVisabilty(1);
+                                  },
+                                  validate: (value) {
+                                    if (value!.isEmpty) {
+                                      return AppString.emptyPassword;
+                                    } else {
+                                      if (value.length < 6) {
+                                        return AppString.notValidPassword;
+                                      }
+                                    }
+                                    return null;
+                                  },
+                                  inputType: TextInputType.visiblePassword),
+                              SizedBox(
+                                height: rheight(context) / 50,
+                              ),
+                              MyFormField(
+                                  hint: 'Confirm Password',
+                                  prefix: Icons.lock_outline_rounded,
+                                  controller:
+                                      cubit.confirmResetpasswordController,
+                                  isPassword: cubit.isPassword2,
+                                  suffix: cubit.isPassword2
+                                      ? Icons.visibility_off_outlined
+                                      : Icons.visibility_outlined,
+                                  suffixPress: () {
+                                    cubit.changeVisabilty(2);
+                                  },
+                                  validate: (value) {
+                                    if (value!.isEmpty) {
+                                      return AppString.emptyPassword;
+                                    } else {
+                                      if (cubit.confirmResetpasswordController
+                                              .text !=
+                                          cubit.resetpasswordController.text) {
+                                        return AppString
+                                            .notValidConfirmPassword;
+                                      }
+                                    }
+                                    return null;
+                                  },
+                                  inputType: TextInputType.visiblePassword),
+                              SizedBox(
+                                height: rheight(context) / 10,
+                              ),
+                              MyButton(
+                                  child: Text(
+                                    'Conirm',
+                                    style: AppTextStyle.buttonText,
+                                  ),
+                                  onPressed: () {
+                                    if (cubit.resetPasswordformKey.currentState!
+                                        .validate()) {
+                                      navigatAndReplacement(context,
+                                          screen: const SignInScreen());
+                                    }
+                                  })
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/modules/SignUp/sign_up_screen.dart
+++ lib/modules/SignUp/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mega_store/bloc/register/register_cubit.dart';
@@ -31,144 +32,149 @@ class SignUpScreen extends StatelessWidget {
               padding: EdgeInsets.only(top: rheight(context) / 30),
               child: SingleChildScrollView(
                 physics: const BouncingScrollPhysics(),
-                child: Column(
-                  children: [
-                    const LogoWithText(
-                        firstText: AppString.letsStart,
-                        secondText: AppString.create),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: rwidth(context) / 25,
-                          vertical: rheight(context) / 30),
-                      child: Form(
-                        key: cubit.formKey,
-                        child: Column(
-                          children: [
-                            MyFormField(
-                                hint: 'Full Name',
-                                prefix: Icons.person_outline,
-                                controller: cubit.fullNameController,
-                                inputType: TextInputType.name,
-                                validate: (value) {
-                                  if (value!.isEmpty) {
-                                    return AppString.emptyfullName;
-                                  }
-                                  return null;
-                                }),
-                            SizedBox(
-                              height: rheight(context) / 80,
-                            ),
-                            MyFormField(
-                                hint: 'Your Email',
-                                prefix: Icons.mail_outline_outlined,
-                                controller: cubit.emailController,
-                                inputType: TextInputType.emailAddress,
-                                validate: (value) {
-                                  if (value!.isEmpty) {
-                                    return AppString.emptyEmail;
-                                  } else {
-                                    if (!value.contains('@')) {
-                                      return AppString.notValidEmail;
+                child: FadeInRight(
+                  child: Column(
+                    children: [
+                      const LogoWithText(
+                          firstText: AppString.letsStart,
+                          secondText: AppString.create),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: rwidth(context) / 25,
+                            vertical: rheight(context) / 30),
+                        child: Form(
+                          key: cubit.formKey,
+                          child: Column(
+                            children: [
+                              MyFormField(
+                                  hint: 'Full Name',
+                                  prefix: Icons.person_outline,
+                                  controller: cubit.fullNameController,
+                                  inputType: TextInputType.name,
+                                  validate: (value) {
+                                    if (value!.isEmpty) {
+                                      return AppString.emptyfullName;
                                     }
-                                  }
-                                  return null;
-                                }),
-                            SizedBox(
-                              height: rheight(context) / 80,
-                            ),
-                            MyFormField(
-                                hint: 'Your Phone',
-                                prefix: Icons.call,
-                                controller: cubit.phoneController,
-                                inputType: TextInputType.phone,
-                                validate: (value) {
-                                  if (value!.isEmpty) {
-                                    return AppString.emptyPhone;
-                                  }
-                                  return null;
-                                }),
-                            SizedBox(
-                              height: rheight(context) / 80,
-                            ),
-                            MyFormField(
-                                hint: 'Password',
-                                prefix: Icons.lock_outline,
-                                controller: cubit.passwordController,
-                                inputType: TextInputType.visiblePassword,
-                                isPassword: true,
-                                validate: (value) {
-                                  if (value!.isEmpty) {
-                                    return AppString.emptyPassword;
-                                  } else {
-                                    if (value.length < 6) {
-                                      return AppString.notValidPassword;
+                                    return null;
+                                  }),
+                              SizedBox(
+                                height: rheight(context) / 80,
+                              ),
+                              MyFormField(
+                                  hint: 'Your Email',
+                                  prefix: Icons.mail_outline_outlined,
+                                  controller: cubit.emailController,
+                                  inputType: TextInputType.emailAddress,
+                                  validate: (value) {
+                                    if (value!.isEmpty) {
+                                      return AppString.emptyEmail;
+                                    } else {
+                                      if (!value.contains('@')) {
+                                        return AppString.notValidEmail;
+                                      }
                                     }
-                                  }
-                                  return null;
-                                }),
-                            SizedBox(
-                              height: rheight(context) / 80,
-                            ),
-                            MyFormField(
-                                hint: 'Confirm password',
-                                prefix: Icons.lock_outline,
-                                controller: cubit.confirmPasswordController,
-                                inputType: TextInputType.visiblePassword,
-                                validate: (value) {
-                                  if (value!.isEmpty) {
-                                    return AppString.emptyConfirmPassword;
-                                  } else {
-                                    if (cubit.confirmPasswordController.text !=
-                                        cubit.passwordController.text) {
-                                      return AppString.notValidConfirmPassword;
+                                    return null;
+                                  }),
+                              SizedBox(
+                                height: rheight(context) / 80,
+                              ),
+                              MyFormField(
+                                  hint: 'Your Phone',
+                                  prefix: Icons.call,
+                                  controller: cubit.phoneController,
+                                  inputType: TextInputType.phone,
+                                  validate: (value) {
+                                    if (value!.isEmpty) {
+                                      return AppString.emptyPhone;
                                     }
-                                  }
-                                  return null;
-                                }),
-                            SizedBox(
-                              height: rheight(context) / 30,
-                            ),
-                            MyButton(
-                                child: Text(
-                                  'Sign Up',
-                                  style: AppTextStyle.buttonText,
-                                ),
-                                onPressed: () {
-                                  if (cubit.formKey.currentState!.validate()) {
-                                    navigatAndReplacement(context,
-                                        screen: const HomeLayout());
-                                  }
-                                }),
-                            SizedBox(
-                              height: rheight(context) / 60,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'have a account? ',
-                                  style: AppTextStyle.greyBold
-                                      .copyWith(fontWeight: FontWeight.w400),
-                                ),
-                                InkWell(
-                                  onTap: () {
-                                    navigatTo(context,
-                                        screen: const SignInScreen());
-                                  },
-                                  child: Ink(
-                                    child: Text('Sign In',
-                                        style: AppTextStyle.greyBold.copyWith(
-                                          color: AppColors.buttonColor,
-                                        )),
+                                    return null;
+                                  }),
+                              SizedBox(
+                                height: rheight(context) / 80,
+                              ),
+                              MyFormField(
+                                  hint: 'Password',
+                                  prefix: Icons.lock_outline,
+                                  controller: cubit.passwordController,
+                                  inputType: TextInputType.visiblePassword,
+                                  isPassword: true,
+                                  validate: (value) {
+                                    if (value!.isEmpty) {
+                                      return AppString.emptyPassword;
+                                    } else {
+                                      if (value.length < 6) {
+                                        return AppString.notValidPassword;
+                                      }
+                                    }
+                                    return null;
+                                  }),
+                              SizedBox(
+                                height: rheight(context) / 80,
+                              ),
+                              MyFormField(
+                                  hint: 'Confirm password',
+                                  prefix: Icons.lock_outline,
+                                  controller: cubit.confirmPasswordController,
+                                  inputType: TextInputType.visiblePassword,
+                                  validate: (value) {
+                                    if (value!.isEmpty) {
+                                      return AppString.emptyConfirmPassword;
+                                    } else {
+                                      if (cubit
+                                              .confirmPasswordController.text !=
+                                          cubit.passwordController.text) {
+                                        return AppString
+                                            .notValidConfirmPassword;
+                                      }
+                                    }
+                                    return null;
+                                  }),
+                              SizedBox(
+                                height: rheight(context) / 30,
+                              ),
+                              MyButton(
+                                  child: Text(
+                                    'Sign Up',
+                                    style: AppTextStyle.buttonText,
                                   ),
-                                ),
-                              ],
-                            )
-                          ],
+                                  onPressed: () {
+                                    if (cubit.formKey.currentState!
+                                        .validate()) {
+                                      navigatAndReplacement(context,
+                                          screen: const HomeLayout());
+                                    }
+                                  }),
+                              SizedBox(
+                                height: rheight(context) / 60,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'have a account? ',
+                                    style: AppTextStyle.greyBold
+                                        .copyWith(fontWeight: FontWeight.w400),
+                                  ),
+                                  InkWell(
+                                    onTap: () {
+                                      navigatTo(context,
+                                          screen: const SignInScreen());
+                                    },
+                                    child: Ink(
+                                      child: Text('Sign In',
+                                          style: AppTextStyle.greyBold.copyWith(
+                                            color: AppColors.buttonColor,
+                                          )),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             )),
